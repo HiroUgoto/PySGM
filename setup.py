dff33fc4f8f8228f340b0915c306e5a67f08067a
Curima <kurimm_sin_cos@yahoo.co.jp>
2022-03-28 15:34:24 +0900

    pip対応

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from glob import glob
+from os.path import basename
+from os.path import splitext
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PySGM",
+    version="0.1.0",
+    author="goto hiroyuki",
+    author_email="goto@catfish.dpri.kyoto-u.ac.jp",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+
+    python_requires='>=3.5',
+)
