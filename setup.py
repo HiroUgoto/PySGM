0e9e1418d5dd0e675d609b9ed95b8de5110a1bf4
HiroUgoto <goto@catfish.dpri.kyoto-u.ac.jp>
2022-07-25 10:27:21 +0900

    0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pySGM",
-    version="0.1.2",
+    version="0.1.3",
     author="goto hiroyuki",
     author_email="goto@catfish.dpri.kyoto-u.ac.jp",
     description="Pysgm'",
